efd167459a991426c42033a63b5e1c7874902c53
tymondragon <tymondragon@gmail.com>
2022-04-01 20:37:00 -0600

    add axios

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -4,23 +4,16 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
-
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
-
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
+import App from '../components/App'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
-  )
+    <Router>
+      <Routes>
+        <Route path='*' element={<App />} />
+      </Routes>
+    </Router>,
+    document.body.appendChild(document.createElement('div'))
+  );
 })
